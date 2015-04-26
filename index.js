adbffb0ce97e84f4c664ab0127827061296c4cc6
Jonathan Ong <jonathanrichardong@gmail.com>
2015-04-25 23:38:35 -0700

    mh: fix up js styling

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ var pHash = require('./build/Release/pHash');
 
 module.exports = pHashImage;
 
+/**
+ * http://www.phash.org/docs/design.html
+ * Returns either an 8-byte buffer or 64-bit integer as a string.
+ *
+ * @param {String} filename
+ * @param {Boolean} return bigint
+ * @param {Function} callback(err, Buffer)
+ * @return Promise => Buffer
+ */
+
 function pHashImage(file, returnBigInt, cb) {
   if (typeof returnBigInt === 'function') {
     cb = returnBigInt;
@@ -28,17 +38,18 @@ function pHashImage(file, returnBigInt, cb) {
 }
 
 /**
- * Export additional function
- * @param  {String}   file     - filename, sadly pHash lib doesn't support
- *                             	 accepting raw buffer
- * @param  {Function} callback <err, Buffer[hex]>
+ * http://www.phash.org/docs/design.html
+ * Returns an 9-byte buffer.
+ *
+ * @param {String} filename
+ * @param {Function} callback(err, Buffer)
+ * @return Promise => Buffer
  */
+
 pHashImage.mh = function (file, callback) {
   var promise = new Promise(function (resolve, reject) {
     pHash.imageHashMH(file, function (err, hash) {
-      if (err) {
-        return reject(err);
-      }
+      if (err) return reject(err);
 
       // optimize useless conversion?
       resolve(new Buffer(hash.slice(2), 'hex'));
